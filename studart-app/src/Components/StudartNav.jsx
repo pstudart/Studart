628e1b876d92ccbe7dc57e29b801e6b86b026222
pstudart <patrickstudart@gmail.com>
2019-10-03 21:59:36 -0400

    studartNav added

--- studart-app/src/Components/StudartNav.jsx
+++ studart-app/src/Components/StudartNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 class StudartNav extends React.Component{
     render(){
@@ -6,12 +6,12 @@ class StudartNav extends React.Component{
 
         <nav class="flex-nav">
             <ul>
-                <li><a href="#">item 01</a></li>
-                <li><a href="#">item 02</a></li>
-                <li><a href="#">item 03</a></li>
-                <li><a href="#">item 04</a></li>
-                <li><a href="#">item 05</a></li>
-                <li><a href="#">item 06</a></li>
+                <li><a href="www.google.com">item 01</a></li>
+                <li><a href="www.google.com">item 02</a></li>
+                <li><a href="www.google.com">item 03</a></li>
+                <li><a href="www.google.com">item 04</a></li>
+                <li><a href="www.google.com">item 05</a></li>
+                <li><a href="www.google.com">item 06</a></li>
                 <li class="social">
                     <a href="http://twitter.com/patrickstudart">
                         <i class="fa fa-twitter"></i></a>
