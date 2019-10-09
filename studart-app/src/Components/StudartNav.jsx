a46a270418ec5915f41109ec4ad5d0638c726ca9
Patrick Studart <pstudart@SYNESISINTL.COM>
2019-10-09 09:50:06 -0400

    progress

--- studart-app/src/Components/StudartNav.jsx
+++ studart-app/src/Components/StudartNav.jsx
@@ -6,9 +6,10 @@ class StudartNav extends React.Component{
 
         <nav className="flex-nav">
             <ul>
-                <li><a href="www.google.com">Infowise Training</a></li>
-                <li><a href="www.google.com">Nintex Training</a></li>
-                <li><a href="www.google.com">API Training</a></li>
+                <li><a href="www.google.com">Event Handlers</a></li>
+                <li><a href="www.google.com">Workflows</a></li>
+                <li><a href="www.google.com">API's</a></li>
+                <li><a href="www.google.com">My Resume</a></li>
                 
                 
                 
@@ -23,10 +24,6 @@ class StudartNav extends React.Component{
                 <li className="social">
                     <a href="http://github.com/patrickstudart">
                         <i className="fa fa-github"></i></a>
-                </li>
-                <li className="social">
-                    <a href="http://twitter.com/patrickstudart">
-                        <i className="fa fa-instagram"></i></a>
                 </li>
             </ul>
         </nav>
