fca3e2f58d9c494e92b5958150c42f6204f34e30
Patrick Studart <pstudart@SYNESISINTL.COM>
2019-10-10 09:24:35 -0400

    allowed the container to have children to modularize it

--- studart-app/src/App.js
+++ studart-app/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import StudartNav from './Components/StudartNav'
 import StudartLogo from './Components/StudartLogo'
 import StudartVideoContainer from './Components/StudartVideoContainer'
+import StudartVideo from './Components/StudartVideo'
+import uniqueId from './Images/Thumbnails/CreateUniqueID.png'
+
+
 
 
 
@@ -11,8 +15,13 @@ function App() {
   return (
     <div className="App">
       <StudartLogo />
-<StudartNav />
-<StudartVideoContainer />
+        <StudartNav />
+        <StudartVideoContainer>
+          <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}></StudartVideo>
+          <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}></StudartVideo>
+          <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}></StudartVideo>
+          <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}></StudartVideo>
+        </StudartVideoContainer>
 
     </div>
   );
--- studart-app/src/Components/StudartVideoContainer.jsx
+++ studart-app/src/Components/StudartVideoContainer.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import StudartVideo from './StudartVideo'
-import drone from '../Images/Thumbnails/drone.png'
-import uniqueId from '../Images/Thumbnails/CreateUniqueID.png'
+
 
 class StudartVideoContainer extends React.Component{
     render(){
@@ -10,19 +8,7 @@ class StudartVideoContainer extends React.Component{
     <div class="VideosWrapper">
             <h3 class="VideosHeaderText">My Video Collection</h3>
         <div class="Videos">
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="Learn to Create a Unique ID in SharePoint." ThumbnailImage={uniqueId}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={uniqueId}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-            <StudartVideo VideoUrl="www.google.com" AltDesc="infowise training" Description="getting started with infowise" ThumbnailImage={drone}/>
-
+        {this.props.children}
         </div>
     </div>
         )
