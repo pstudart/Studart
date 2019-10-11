2cd3e64f29ff348385fd37f817ad655a325196bd
Patrick Studart <pstudart@SYNESISINTL.COM>
2019-10-11 15:15:12 -0400

    updates to logo

--- studart-app/src/Components/StudartLogo.jsx
+++ studart-app/src/Components/StudartLogo.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Logo from '../Images/MainLogo/Logo_500w150h.png'
+import BigLogo from '../Images/MainLogo/Logo_500w150h.png'
+import SmallLogo from '../Images/MainLogo/Logo_200w75h.png'
 
 class StudartLogo extends React.Component{
     render(){
         return(
 
        <div className="mainlogo">
-           <p><img src={Logo} alt="Logo" /></p>
+           <p><img className="logo-big" src={BigLogo} alt="Logo" /></p>
+           <p><img className="logo-small" src={SmallLogo} alt="SmallLogo" /></p>
        </div>
         )
     }
